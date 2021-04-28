7a709e9a6d5ad4e461804527198e34561d8fe1b3
MansaMusa55 <musax025@gmail.com>
2021-04-28 15:42:58 -0500

    Test

---
+++ MoviePro/Models/Cast.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviePro.Models
+{
+    public class Cast
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int CastId { get; set; }
+        public string Department { get; set; }
+        public string Name { get; set; }
+        public string Character { get; set; }
+        public int Order { get; set; }
+
+        //Profile Image
+        [Display(Name = "Profile Pic")]
+        public byte[] Profile { get; set; }
+
+        public string ContentType { get; set; }
+       
+    }
+}
---
+++ MoviePro/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviePro.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        //The movieID from TMBD
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string TagLine { get; set; }
+        public string Overview { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Movie Poster")]
+
+        public byte[] Poster { get; set; }
+        public string ContentType { get; set; }
+
+        [Display(Name = "Background Image")]
+        public byte[] BGImage { get; set; }
+        public string BGContentType { get; set; }
+
+        //Public Url of the trailer
+        [Display(Name = "Trailer URL")]
+        public string Trailer { get; set; }
+        public ICollection<Cast> Cast { get; set; } = new HashSet<Cast>();
+    }
+}
